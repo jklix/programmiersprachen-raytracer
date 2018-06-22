ee696e53745998287343094a68011672a7ca9dcd
Judith Klix <judith.klix@uni-weimar.de>
2018-06-22 19:13:22 +0200

    Aufgabe 5.5

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 #include <glm/vec3.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtx/intersect.hpp>
 
 
 #include "../framework/box.hpp"
@@ -65,6 +67,28 @@ TEST_CASE("print", "[shape, box, sphere]")
 
   std::cout << s1 << "\n";
   std::cout << s2 << "\n";
+}
+
+TEST_CASE ("intersect_ray_sphere", "[intersect]")
+{
+  //Ray
+  glm::vec3 ray_origin{0.0f, 0.0f, 0.0f};1
+  //ray direction has to be normalized !
+  //you can use:
+  // v = glm::normalize(some_vector)
+  glm::vec3 ray_direction {0.0f, 0.0f, 1.0f};
+
+  //Sphere
+  glm::vec3 sphere_center{0.0f, 0.0f,  5.0f};
+  float sphere_radius {1.0f};
+
+  float distance = 0.0f;
+  auto result = glm::intersectRaySphere(
+    ray_origin, ray_direction,
+    sphere_center,
+    sphere_radius * sphere_radius, //squared radius !!!
+    distance);
+  REQUIRE(distance == Approx(4.0f));
 }
 
 
